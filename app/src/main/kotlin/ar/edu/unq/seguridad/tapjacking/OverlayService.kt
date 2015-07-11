c916948ef808dcaaef823937811e69f36f66bbdf
ariel <alvarez.ariel.work@gmail.com>
2015-07-11 03:14:26 -0300

    Added overlay button

--- app/src/main/kotlin/ar/edu/unq/seguridad/tapjacking/OverlayService.kt
+++ app/src/main/kotlin/ar/edu/unq/seguridad/tapjacking/OverlayService.kt
@@ -5,14 +5,20 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.*
 import android.widget.LinearLayout
 import kotlin.properties.Delegates
 
 public class OverlayService : Service() {
 
-    val overlay by Delegates.lazy { LinearLayout(this) }
+    val overlay by Delegates.lazy {
+        val inflater = getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.service_alert, null)
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -20,20 +26,34 @@ public class OverlayService : Service() {
         overlay.setBackgroundColor(TRANSLUCENT_RED)
 
         val params = WindowManager.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT)
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            TYPE_SYSTEM_ALERT,
+            FLAG_NOT_TOUCHABLE,
+            PixelFormat.TRANSLUCENT
+        )
 
+        overlay.findViewById(R.id.first_button).setOnClickListener {
+            it.setVisibility(View.GONE)
+        }
         (getSystemService(Context.WINDOW_SERVICE) as WindowManager).addView(overlay, params)
+
+        sendText("Texto que quiero compartir")
+    }
+
+    private fun sendText(text:String) {
+        val sendIntent = Intent();
+        sendIntent.setAction(Intent.ACTION_SEND)
+        sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text)
+        sendIntent.setType("text/plain")
+        startActivity(sendIntent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         (getSystemService(Context.WINDOW_SERVICE) as WindowManager).removeView(overlay)
     }
-
 
     val TRANSLUCENT_RED = -1996554240
     override fun onBind(i: Intent): IBinder? {return null}
